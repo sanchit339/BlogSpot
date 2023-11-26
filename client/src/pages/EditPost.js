27bfc5aa202cff1ad92075a8a6ec532d6a0aaf11
Sanchit Ingale <sanchitingale09@gmail.com>
2023-11-26 23:38:39 +0530

    Changes

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('http://localhost:4000/post', {
+    const response = await fetch('https://backend-sanchit339.vercel.app/post', {
       method: 'PUT',
       body: data,
       credentials: 'include',
